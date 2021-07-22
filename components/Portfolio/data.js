1d6b309e182ba45e8ce7c60dcc700e1e8ff7dcab
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-07-22 17:49:04 +0200

    Webp Images in slider

--- components/Portfolio/data.js
+++ components/Portfolio/data.js
@@ -1,39 +1,21 @@
 export const data = [
 
     {
-        image: '/images/netflix.png',
+        image: '/images/netflix.webp',
         title: 'Netflix Clone',
         id: 1,
         content: 'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For animation i used a Framer Motion',
     },
     {
-        image: '/images/ohhsite.png',
+        image: '/images/ohhsite.webp',
         title: 'OhhSite',
         id: 2,
         content: 'Website of the company I work with. I used a Next.js and Strapi. All animations was created in Framer Motion.',
     },
     {
-        image: '/images/imassage.png',
+        image: '/images/imassage.webp',
         title: 'Imessage Clone',
         id: 3,
         content: 'Just a simple clone, made in React.js, Redux. For backend i used Node.js, Express.js with Pusher solutions for real-time chat. Google Auth is implemented with Firebase.',
-    },
-    {
-        image: '/images/netflix.png',
-        title: 'Netflix Cole',
-        id: 4,
-        content: 'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For animation i used a Framer Motion',
-    },
-    {
-        image: '/images/netflix.png',
-        title: 'Netflix Cole',
-        id: 5,
-        content: 'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For animation i used a Framer Motion',
-    },
-    {
-        image: '/images/netflix.png',
-        title: 'Netflix Cole',
-        id: 6,
-        content: 'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For animation i used a Framer Motion',
     },
 ]
