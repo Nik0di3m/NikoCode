d323317fd4896bcf23a5937209846d3b5a35c25f
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-08-14 19:23:26 +0200

    portfolio airbnb

--- components/Portfolio/data.js
+++ components/Portfolio/data.js
@@ -7,47 +7,55 @@ export const data = [
         link: 'https://dzomakeup.pl'
     },
     {
+        image: '/images/airbnbcolne.webp',
+        title: 'Airbnb Clone',
+        id: 1,
+        content: 'Airbnb clone made with Next.js and Strapi. For CSS i used a tailwindcss. Website has a working serach bar, but for now u can only search places in Warsaw',
+        link: 'https://air-bnb-two.vercel.app/'
+    },
+    {
         image: '/images/googleDocs.webp',
         title: 'Google Docs Clone',
-        id: 1,
+        id: 2,
         content: 'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For Auth I used Next-Auth. For now you can create Docs and edit. Frontend was built on material-taillwind-ui and tailwind',
         link: 'https://google-docs-nine.vercel.app'
     },
     {
         image: '/images/facebook.webp',
         title: 'Facebook Clone',
-        id: 2,
+        id: 3,
         content: 'Facebook Clone, I used React with Firebase to make this application. I tried to clone Facebook Ui. For CSS, I used styled-components🔥 ',
         link: 'https://facebook-5b249.firebaseapp.com'
     },
     {
         image: '/images/imassage.webp',
         title: 'Imessage Clone',
-        id: 3,
+        id: 4,
         content: 'Just a simple clone, made in React.js, Redux. For backend i used Node.js, Express.js with Pusher solutions for real-time chat. Google Auth is implemented with Firebase.',
         link: 'https://imessage-aa5b1.firebaseapp.com'
     },
     {
         image: '/images/spotifyClone.jpg',
         title: 'Spotify Clone',
-        id: 4,
+        id: 5,
         content: 'I tried to clone the mechanics of spotify. The frontend of the application was created with React Native, I used AWS Amplify for the backend. You can test the application if you have Expo Go on your phone, just scan the QR code 🔥',
         link: 'https://expo.dev/@nikodex/SpotifyClone'
     },
     {
         image: '/images/signalClone.jpg',
         title: 'Signal Clone',
-        id: 5,
+        id: 6,
         content: 'Another clone, I made it in React Native and Firebase. This is a simple group chat application. I was modeled on the Signal App. The entire backend is based on Firebase🔥. You can test the application if you have Expo Go on your phone, just scan the QR code 🔥',
         link: 'https://expo.dev/@nikodex/signal'
     },
     {
         image: '/images/slack.webp',
         title: 'Slack Clone',
-        id: 6,
+        id: 7,
         content: 'Another clone, I made it in React with Firebase🔥. The website is not adapted to mobile devices, maybe I will add responsiveness in the future',
         link: 'https://slack-e91db.firebaseapp.com'
     },
+
 
 
 ]
