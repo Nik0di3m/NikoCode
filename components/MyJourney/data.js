e483b7f4750beaa07758bcbb0675065d1a918289
Nik0di3m <norbert.waleszczyk@gmail.com>
2021-08-21 15:25:52 +0200

    fix

--- components/MyJourney/data.js
+++ components/MyJourney/data.js
@@ -1,43 +1,42 @@
 export const data = [
     {
-        text: "Start learning web developing",
-        data: '2017',
+        text: 'Start learning web developing',
+        data: '2019',
         id: '1',
     },
     {
-        text: "WordPress freelancer",
-        data: '2018',
+        text: 'WordPress freelancer',
+        data: '2019 Q2',
         id: '2',
     },
     {
-        text: "Meet with Python and Django",
-        data: '2019',
+        text: 'Meet with Python and Django',
+        data: '2019 Q2',
         id: '3',
     },
     {
-        text: "Start with Js and React",
-        data: '2020',
+        text: 'Start with Js and React',
+        data: '2020 Q4',
         id: '4',
     },
     {
-        text: "Start working as React freelancer",
-        data: '2020 Q4',
+        text: 'Start working as React freelancer',
+        data: '2021 Q1',
         id: '5',
     },
     {
-        text: "Start learning React Native",
+        text: 'Start learning React Native',
         data: '2021 Q2',
         id: '6',
     },
     {
-        text: "Deploy first mobile App",
+        text: 'Deploy first mobile App',
         data: '2021 Q4',
         id: '7',
     },
     {
-        text: "Who knows🔥",
+        text: 'Who knows🔥',
         data: '2022',
         id: '8',
     },
-
 ]
