1a918e1896fe4725323cad54a65c86c1cb5d0185
Nik0di3m <norbert.waleszczyk@gmail.com>
2021-08-23 11:21:33 +0200

    phone number add

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,25 +1,23 @@
 import Image from 'next/image'
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
-import axios from 'axios';
+import axios from 'axios'
 const Contact = () => {
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit, reset } = useForm()
     const onSubmit = async (data) => {
         let config = {
-            method: "post",
-            url: "/api/mail",
+            method: 'post',
+            url: '/api/mail',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
-            data: data
-        };
+            data: data,
+        }
         try {
-
-            const res = await axios(config);
+            const res = await axios(config)
             if (res.status === 200) {
                 reset()
             }
-
         } catch (error) {
             console.log(error)
         }
@@ -33,7 +31,7 @@ const Contact = () => {
                     <div className="flex flex-col h-full justify-around">
                         {/* email and phone */}
                         <div className="flex items-center mb-6">
-                            <MailIcon className='h-12 w-12 text-orange' />
+                            <MailIcon className="h-12 w-12 text-orange" />
                             <div className="ml-2">
                                 <h3 className="text-xl font-medium">Email</h3>
                                 <p>norbert.waleszczyk@icloud.com</p>
@@ -43,41 +41,65 @@ const Contact = () => {
                             <PhoneIcon className="h-12 w-12 text-orange" />
                             <div className="ml-2">
                                 <h3 className="text-xl font-medium">Phone</h3>
-                                <p>+48 987 654 321</p>
+                                <p>+48 574 323 453</p>
                             </div>
                         </div>
                     </div>
                     <div className="flex">
                         {/* image */}
-                        <Image src="/images/send.svg" width={350} height={350} alt="Sending message" />
+                        <Image
+                            src="/images/send.svg"
+                            width={350}
+                            height={350}
+                            alt="Sending message"
+                        />
                     </div>
                 </div>
                 <div className="lg:w-2/4 text-light">
                     {/* form */}
                     <form
                         onSubmit={handleSubmit(onSubmit)}
-                        className="flex flex-col items-center mb-4 md:inline-block lg:inline-block lg:mb-0">
+                        className="flex flex-col items-center mb-4 md:inline-block lg:inline-block lg:mb-0"
+                    >
                         <input
-                            {...register("name", {
+                            {...register('name', {
                                 required: true,
                             })}
-                            type="text" placeholder="Name:" className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2" />
+                            type="text"
+                            placeholder="Name:"
+                            className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2"
+                        />
                         <input
-                            {...register("email", {
+                            {...register('email', {
                                 required: true,
                             })}
-                            type="email" placeholder="Email:" className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2" />
+                            type="email"
+                            placeholder="Email:"
+                            className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2"
+                        />
                         <input
-                            {...register("topic", {
+                            {...register('topic', {
                                 required: true,
                             })}
-                            type="text" placeholder="Topic:" className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2" />
+                            type="text"
+                            placeholder="Topic:"
+                            className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2"
+                        />
                         <textarea
-                            {...register("message", {
+                            {...register('message', {
                                 required: true,
                             })}
-                            type="text" placeholder="Messege:" style={{ height: '22rem' }} className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2 h-80 resize-none" />
-                        <button type="submit" className="bg-orange p-2 w-36 rounded-2xl hover:scale-110 duration-200">Send</button>
+                            type="text"
+                            placeholder="Messege:"
+                            style={{ height: '22rem' }}
+                            className="rounded-2xl bg-orange p-[3px] w-full placeholder-light outline-none mb-2 pl-2 h-80 resize-none"
+                        />
+                        <button
+                            type="submit"
+                            className="bg-orange p-2 w-36 rounded-2xl hover:scale-110 duration-200"
+                        >
+                            Send
+                        </button>
                     </form>
                 </div>
             </div>
