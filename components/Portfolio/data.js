b874d52efb29ae2aa2fd8d763c5fa5991fad8255
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-09-06 13:06:09 +0200

    data added

--- components/Portfolio/data.js
+++ components/Portfolio/data.js
@@ -8,9 +8,25 @@ export const data = [
         link: 'https://ohh-site3-0.vercel.app/',
     },
     {
+        image: '/images/NextFireshop.webp',
+        title: 'FireShop',
+        id: 1,
+        content:
+            "Ecommerce alpha project with Next.js, Firebase and custom CMS made by my FireShop. Site is in early stage, I'm developing it as soon as I have time",
+        link: 'https://next-js-firebase-ecommerce.vercel.app/',
+    },
+    {
+        image: '/images/alekrawaty.webp',
+        title: 'Alekrawaty.pl',
+        id: 2,
+        content:
+            'Online store built with PrestaShop. Responsive design, with custom CSS.',
+        link: 'https://alekrawaty.pl',
+    },
+    {
         image: '/images/dzomakeupPortfolio.webp',
         title: 'Dzomakeup.pl',
-        id: 1,
+        id: 3,
         content:
             'Website adoptable to all devices, with ui copmonents. Making in Next.js with Strapi CMS. For frontend I used a tailwind CSS. All content is rendering from CMS',
         link: 'https://dzomakeup.pl',
@@ -18,7 +34,7 @@ export const data = [
     {
         image: '/images/airbnbclone.webp',
         title: 'Airbnb Clone',
-        id: 2,
+        id: 4,
         content:
             'Airbnb clone made with Next.js and Strapi. For CSS i used a tailwindcss. Website has a working serach bar, but for now u can only search places in Warsaw',
         link: 'https://air-bnb-two.vercel.app/',
@@ -26,26 +42,10 @@ export const data = [
     {
         image: '/images/googleDocs.webp',
         title: 'Google Docs Clone',
-        id: 3,
+        id: 5,
         content:
             'Website adoptable to all devices, with ui copmonents. Making in Next.js with Firebase backend. For Auth I used Next-Auth. For now you can create Docs and edit. Frontend was built on material-taillwind-ui and tailwind',
         link: 'https://google-docs-nine.vercel.app',
-    },
-    {
-        image: '/images/facebook.webp',
-        title: 'Facebook Clone',
-        id: 4,
-        content:
-            'Facebook Clone, I used React with Firebase to make this application. I tried to clone Facebook Ui. For CSS, I used styled-components🔥 ',
-        link: 'https://facebook-5b249.firebaseapp.com',
-    },
-    {
-        image: '/images/imassage.webp',
-        title: 'Imessage Clone',
-        id: 5,
-        content:
-            'Just a simple clone, made in React.js, Redux. For backend i used Node.js, Express.js with Pusher solutions for real-time chat. Google Auth is implemented with Firebase.',
-        link: 'https://imessage-aa5b1.firebaseapp.com',
     },
     {
         image: '/images/spotifyClone.jpg',
@@ -56,19 +56,28 @@ export const data = [
         link: 'https://expo.dev/@nikodex/SpotifyClone',
     },
     {
+        image: '/images/facebook.webp',
+        title: 'Facebook Clone',
+        id: 7,
+        content:
+            'Facebook Clone, I used React with Firebase to make this application. I tried to clone Facebook Ui. For CSS, I used styled-components🔥 ',
+        link: 'https://facebook-5b249.firebaseapp.com',
+    },
+    {
+        image: '/images/imassage.webp',
+        title: 'Imessage Clone',
+        id: 8,
+        content:
+            'Just a simple clone, made in React.js, Redux. For backend i used Node.js, Express.js with Pusher solutions for real-time chat. Google Auth is implemented with Firebase.',
+        link: 'https://imessage-aa5b1.firebaseapp.com',
+    },
+
+    {
         image: '/images/signalClone.jpg',
         title: 'Signal Clone',
-        id: 7,
+        id: 9,
         content:
             'Another clone, I made it in React Native and Firebase. This is a simple group chat application. I was modeled on the Signal App. The entire backend is based on Firebase🔥. You can test the application if you have Expo Go on your phone, just scan the QR code 🔥',
         link: 'https://expo.dev/@nikodex/signal',
-    },
-    {
-        image: '/images/slack.webp',
-        title: 'Slack Clone',
-        id: 8,
-        content:
-            'Another clone, I made it in React with Firebase🔥. The website is not adapted to mobile devices, maybe I will add responsiveness in the future',
-        link: 'https://slack-e91db.firebaseapp.com',
     },
 ]
